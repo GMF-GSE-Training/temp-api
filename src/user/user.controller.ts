8123133e350d05b8a9757eda869a11a96c09bee5
Aminudin Abdulloh <aminabdulah01@gmail.com>
2024-08-02 03:40:31 +0700

    implementasi user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,17 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
+import { UserService } from "./user.service";
+import { WebResponse } from "src/model/web.model";
+import { RegisterUserRequest, UserResponse } from "src/model/user.model";
 
 @Controller("/users")
 export class UserController {
-    
+    constructor(private userService: UserService) {}
+
+    @Post()
+    async register(@Body() req: RegisterUserRequest): Promise<WebResponse<UserResponse>> {
+        const result = await this.userService.register(req);
+        return{
+            data: result,
+        }
+    }
 }
