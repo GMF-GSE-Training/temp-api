1334e78e4d966f309af3a46f3a365f060dd30f4c
Aminudin Abdulloh <aminabdulah01@gmail.com>
2024-08-05 21:01:12 +0700

    membuat fitur create user yang hanya dapat dilakukan oleh super admin, supervisor, dan lcu

--- src/common/decorator/role.decorator.ts
+++ src/common/decorator/role.decorator.ts
@@ -1,8 +1,4 @@
 import { SetMetadata } from '@nestjs/common';
 import { Role } from '@prisma/client';
 
-export const Roles = (...roles: Role["role"][]) => {
-    // Normalisasi nama role ke huruf kecil
-    const normalizedRoles = roles.map(role => role.toLowerCase());
-    return SetMetadata('roles', normalizedRoles);
-};
+export const Roles = (...roles: Role["role"][]) => SetMetadata('roles', roles)
--- src/common/guard/role.guard.ts
+++ src/common/guard/role.guard.ts
@@ -20,7 +20,6 @@ export class RoleGuard implements CanActivate {
             return true;
         }
 
-        // Normalisasi roles yang diperlukan menjadi huruf kecil
         const normalizedRequiredRoles = requiredRoles.map(role => role.toLowerCase());
 
         const request = context.switchToHttp().getRequest();
@@ -31,7 +30,6 @@ export class RoleGuard implements CanActivate {
             throw new HttpException('Unauthorized', 401);
         }
 
-        // Ambil role dari database dan normalisasi ke huruf kecil
         const userWithRole = await this.prismaService.user.findUnique({
             where: { id: user.id },
             include: { role: true }
@@ -41,7 +39,7 @@ export class RoleGuard implements CanActivate {
             throw new HttpException('Forbidden', 403);
         }
 
-        const userRoleName = userWithRole.role.role.toLowerCase(); // Normalisasi ke huruf kecil
+        const userRoleName = userWithRole.role.role.toLowerCase();
         const hasRole = normalizedRequiredRoles.includes(userRoleName);
         console.log('User role:', userRoleName, 'Required roles:', normalizedRequiredRoles);
 
