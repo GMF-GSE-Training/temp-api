4aadf6e2d8b43802ae5463ee3fad033591b649d9
Aminudin Abdulloh <aminabdulah01@gmail.com>
2024-08-29 22:06:24 +0700

    memasukan prisma service ke auth module sebagai providers

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { jwtConstants } from '../config/constants';
 import { AuthService } from "./auth.service";
 import { AuthController } from "./auth.controller";
 import { AuthGuard } from "../common/guard/auth.guard";
+import { PrismaService } from "src/common/service/prisma.service";
 
 @Module({
     imports: [
@@ -15,6 +16,7 @@ import { AuthGuard } from "../common/guard/auth.guard";
     providers: [
         AuthService,
         AuthGuard,
+        PrismaService
     ],
     controllers: [AuthController],
 })
