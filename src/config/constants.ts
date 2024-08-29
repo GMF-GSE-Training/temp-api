af6b95562e4841211da795e3d0c9158ad4a45bad
Aminudin Abdulloh <aminabdulah01@gmail.com>
2024-08-30 04:36:02 +0700

    membuat data role dan super admin saat server dijalankan

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,8 @@
 export const jwtConstants = {
     access_token: process.env.ACCESS_TOKEN,
     access_token_expires_in: process.env.ACCESS_TOKEN_EXPIRES_IN,
+    super_admin_no_pegawai: process.env.SUPER_ADMIN_NO_PEGAWAI,
+    super_admin_email: process.env.SUPER_ADMIN_EMAIL,
+    super_admin_name: process.env.SUPER_ADMIN_NAME,
+    super_admin_password: process.env.SUPER_ADMIN_PASSWORD,
 };
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import * as cookieParser from 'cookie-parser';
+import { jwtConstants } from './config/constants';
+import { PrismaService } from './common/service/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
   app.useLogger(logger);
+
+  const prismaService = app.get(PrismaService);
+  await seedDatabase(prismaService);
 
   app.enableCors({
     origin: `*`,
@@ -21,4 +27,66 @@ async function bootstrap() {
   const port = process.env.PORT
   await app.listen(port);
 }
+
+async function seedDatabase(prismaService: PrismaService) {
+  const superAdminNoPegawai = jwtConstants.super_admin_no_pegawai;
+  const superAdminEmail = jwtConstants.super_admin_email;
+  const superAdminName = jwtConstants.super_admin_name;
+  const superAdminPassword = jwtConstants.super_admin_password;
+
+  const superAdminHashedPassword = await bcrypt.hash(superAdminPassword, 10);
+
+  // Check if roles already exist
+  const existingRoles = await prismaService.role.findMany({
+    where: {
+      role: { in: ['super admin', 'supervisor', 'lcu', 'user'] },
+    },
+  });
+
+  // If roles don't exist, create them
+  if (existingRoles.length === 0) {
+    await prismaService.role.createMany({
+      data: [
+        { role: 'super admin' },
+        { role: 'supervisor' },
+        { role: 'lcu' },
+        { role: 'user' },
+      ],
+    });
+  }
+
+  // Check if super admin role exists
+  const superAdminRole = await prismaService.role.findFirst({
+    where: {
+      role: 'super admin',
+    },
+  });
+
+  if (!superAdminRole) {
+    console.log('Super admin role does not exist');
+    return;
+  }
+
+  // Check if super admin user already exists
+  const existingUser = await prismaService.user.findFirst({
+    where: { email: superAdminEmail },
+  });
+
+  if (!existingUser) {
+    await prismaService.user.create({
+      data: {
+        no_pegawai: superAdminNoPegawai,
+        email: superAdminEmail,
+        name: superAdminName,
+        password: superAdminHashedPassword,
+        roleId: superAdminRole.id,
+      },
+    });
+
+    console.log('Super admin user created successfully.');
+  } else {
+    console.log('Super admin user already exists.');
+  }
+}
+
 bootstrap();
