4f5cb1e273e931eb1c7495703b2e36c35d55f8e6
Aminudin Abdulloh <aminabdulah01@gmail.com>
2024-10-16 20:03:04 +0700

    membuat module cot

---
+++ src/cot/cot.controller.ts
@@ -0,0 +1,6 @@
+import { Controller } from "@nestjs/common";
+
+@Controller('/cot')
+export class CotController {
+    
+}
---
+++ src/cot/cot.module.ts
@@ -0,0 +1,11 @@
+import { Module } from "@nestjs/common";
+import { CotService } from "./cot.service";
+import { CotController } from "./cot.controller";
+
+@Module({
+    providers: [CotService],
+    controllers: [CotController],
+})
+export class CotModule {
+
+}
