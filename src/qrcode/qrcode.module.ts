f4a1ec7ee61356917327a0fc6065d3aeb173443f
pangaribowo <fatahillahalifp04@gmail.com>
2025-06-24 22:56:51 +0700

    refactor(qrcode): import FileUploadModule for QR code module
    - Add FileUploadModule to QrCodeModule imports for file handling capabilities.
    - Prepares QR code features for file upload and multi-storage support.

--- src/qrcode/qrcode.module.ts
+++ src/qrcode/qrcode.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { QrCodeService } from './qrcode.service';
 import { CommonModule } from 'src/common/common.module';
+import { FileUploadModule } from '../file-upload/file-upload.module';
 
 @Module({
-  imports: [CommonModule],
+  imports: [CommonModule, FileUploadModule],
   providers: [QrCodeService],
   exports: [QrCodeService],
 })
