fd03dddc65f2ced49f1f8b4194f6544600c20979
pangaribowo <fatahillahalifp04@gmail.com>
2025-06-24 22:49:13 +0700

    refactor(e-sign): import FileUploadModule and export ESignService
    - Add FileUploadModule to ESignModule imports for file handling capabilities.
    - Export ESignService to enable dependency injection in other modules.
    - Improves modularity and prepares for multi-storage support.

--- src/e-sign/e-sign.module.ts
+++ src/e-sign/e-sign.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ESignService } from './e-sign.service';
 import { ESignController } from './e-sign.controller';
+import { FileUploadModule } from '../file-upload/file-upload.module';
 
 @Module({
+  imports: [FileUploadModule],
   providers: [ESignService],
   controllers: [ESignController],
+  exports: [ESignService],
 })
 export class ESignModule {}
