797e61b9a5739320d74a80b08ac02b404ee1b524
pangaribowo <fatahillahalifp04@gmail.com>
2025-06-24 22:46:41 +0700

    refactor(certificate): import FileUploadModule for certificate module
    - Added FileUploadModule to the imports array in CertificateModule.
    - Prepares certificate features for file upload integration and improves modularity.

--- src/certificate/certificate.module.ts
+++ src/certificate/certificate.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CertificateService } from './certificate.service';
 import { CertificateController } from './certificate.controller';
+import { FileUploadModule } from '../file-upload/file-upload.module';
 
 @Module({
+  imports: [FileUploadModule],
   providers: [CertificateService],
   controllers: [CertificateController],
 })
